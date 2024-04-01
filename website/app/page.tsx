bd3ae6c73f355bdf2b1c1750a5f03c61c82e8a44
AmmarMohammed01 <118853010+AmmarMohammed01@users.noreply.github.com>
2024-04-01 15:23:16 -0500

    Update page.tsx

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,92 +1,31 @@
-import { getItem } from './utils'
-
-//export const revalidate = 3600 // revalidate the data at most every hour
- 
-export default async function Page() {
-  const item = await getItem()
-
-  return (
-    <main>
-      <div>
-        { item }
-      </div>
-    </main>
-    )
-}
-
-/* //Front-End
 import './styles.css'
+import Title from './components/title';
+import LocationInterface from './components/location';
+import Fountain from './components/fountain';
+import TotalCounter from './components/total-counter';
+import { getItem } from './prismaFountains'
 
-export default function Page() {
+import { PrismaClient } from '@prisma/client';
 
-  return (<>
-    <InfoBox />
-    <DropdownMenu/>
-            
-  </>);
+const prisma = new PrismaClient();
+
+
+export default async function Page() {
+  const item = await getItem();
+  let fountainArray = JSON.parse(item);
+  //console.log(fountainArray);
+
+  /*let fountainNames = fountainArray.map( (fountain:any) => { 
+    return fountain.name
+  });
+  //console.log(fountainNames); */
+
+  let totalCounter = 0;
+  fountainArray.forEach( (fountain:any) => { totalCounter += fountain.bottleNum });
+
+  return(<>
+    <Title/>
+    {fountainArray.map( (fountain:any, index:Number) => (<Fountain key={index} name={fountain.name} count={fountain.bottleNum}/>) )}
+    <TotalCounter counter={totalCounter}/>
+  </>)
 }
-
-function Title() {
-  return <p className="project-title">Water Bottle Counter</p>;
-}
-
-function SelectedLocation({location}) {
-  return (<>
-  <p className="current-location">Current Location: {location}</p>
-  <p className="global-counter">1587029</p>
-  </>);
-}
-
-function InfoBox() {
-  return (<>
-  <div className="dropdown-middle-placement">
-    <div></div>
-    <div className="dropdown-grid">
-      <Title />
-      <SelectedLocation location="All Locations"/>
-    </div>
-    <div></div>
-  </div>
-    
-  </>);
-}
-
-function DropdownMenu() {
-  return (
-  <>
-    <div className="dropdown-middle-placement">
-      <div></div>
-      <div className="dropdown-grid">
-        <div>
-        <label htmlFor="station-select" className="dropdown-title">Select Refill Station Location:</label>
-        </div>
-
-        <div className="dropdown-grid2">
-          <div>
-            <select className="dropdown-menu" name="stations" id="station-select">
-              <option value="all">All Stations</option>
-              <option value="ECSN-1">ECS North #1</option>
-              <option value="ECSN-2">ECS North #2</option>
-              <option value="ECSW">ECS West</option>
-              <option value="GR">Green Hall</option>
-              <option value="MC">McDermott Library</option>
-              <option value="SU">Student Union</option>
-            </select>
-          </div>
-        
-          <div>
-            <button className="js-selector-button">Get Location</button>
-          </div>
-
-        </div>
-        
-      </div>
-      <div></div>
-    </div>
-  </>
-  );
-}
-
-*/
-
-
