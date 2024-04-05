95d23a681a91db7d15a2c0012e1f6a486bfd7249
AmmarMohammed01 <118853010+AmmarMohammed01@users.noreply.github.com>
2024-04-05 18:29:49 -0500

    Update layout.tsx

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,8 +1,7 @@
-import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700']})
 
 export const metadata: Metadata = {
   title: 'Water Bottle Counter',
@@ -16,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
