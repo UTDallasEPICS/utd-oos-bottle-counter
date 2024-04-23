d05b5e44e875b716ad872958bd229afe1bcc18cd
smitheartc <rcs220002@utdallas.edu>
2024-04-23 15:09:02 -0500

    oasdlla

---
+++ website/app/api/increment/route.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next'   
+import { NextResponse } from 'next/server'
+
+type ResponseData = {
+  message: string
+}
+
+export function GET(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  if (req.method === 'GET') {
+    console.log("This works!!!!")
+    return NextResponse.json({ response: 'it has been incremented' }, { status: 465 })
+} else {
+    return res.status(400).json({ message: 'reCAPTCHA verification failed' })
+  }
+}
+
+export function POST(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    console.log("Post!!!!")
+} else {
+    return res.status(400).json({ message: 'reCAPTCHA verification failed' })
+  }
+}
+
