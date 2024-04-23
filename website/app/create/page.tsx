5ec9a0120bfe53003b160e011262a7d217d74d4b
Ammar Mohammed <ammar.hm321@gmail.com>
2024-04-23 00:06:26 -0500

    Pages and APIs for Create and Delete

--- website/app/create/page.tsx
+++ website/app/create/page.tsx
@@ -1,11 +1,44 @@
 'use client';
+import { FormEvent } from 'react'
+
 export default function CreateFountain() {
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+ 
+    //const formData = new FormData(event.currentTarget.id);
+    console.log((event.currentTarget.elements[0] as HTMLInputElement).value);
+    console.log((event.currentTarget.elements[1] as HTMLInputElement).value);
+
+    const fountain = ({
+      name: (event.currentTarget.elements[0] as HTMLInputElement).value,
+      bottleNum: parseInt( (event.currentTarget.elements[1] as HTMLInputElement).value ),
+    });
+    
+    const res = await fetch('/api/create', {
+      method: 'POST',
+      body: JSON.stringify({fountain: fountain}),
+    });
+
+    // Handle response if necessary
+    const data = await res.json()
+    // ...
+  }
+
   return (<>
-    <p>Fountain Creation Page</p>
-    <input placeholder="Enter Name of Fountain"></input>
-    <input placeholder='Enter current count on counter "if not zero"'></input>
-    <button>Create</button>
-    <p>Fountain "name here" is now in database!</p>
-    <p>Here is the id of new fountain: </p>
+    <form onSubmit={onSubmit}>
+      <legend>Add New Fountain</legend>
+
+      <label htmlFor="fname">
+      Type the location name of the new fountain here (if there are multiple, type a #number following the name)
+      </label>
+      <input id="fname" type="text" placeholder="Fountain Name"/>
+
+      <label htmlFor="fcounter">
+      Type the number of water bottles saved (zero if unused)
+      </label>
+      <input id="fcounter" type="text" placeholder="Initial Counter"/>
+      <button type="submit">Submit</button>
+    </form>
   </>);
 }
--- website/app/delete/page.tsx
+++ website/app/delete/page.tsx
@@ -1,3 +1,35 @@
+'use client';
+import { FormEvent } from 'react';
+
 export default function DeleteFountain() {
-  return <p>Fountain Deletion Page</p>;
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+ 
+    //const formData = new FormData(event.currentTarget.id);
+    console.log((event.currentTarget.elements[0] as HTMLInputElement).value);
+
+    const fountain = ({
+      id: parseInt( (event.currentTarget.elements[0] as HTMLInputElement).value ),
+    });
+    
+    const res = await fetch('/api/delete', {
+      method: 'POST',
+      body: JSON.stringify({fountain: fountain}),
+    });
+
+    // Handle response if necessary
+    const data = await res.json()
+    // ...
+  }
+
+  return (<>
+    <form onSubmit={onSubmit}>
+      <legend>Remove Fountain</legend>
+
+      <label htmlFor="id"> Please type ID of fountain you want to delete </label>
+      <input id="id" type="text" placeholder='ID: e.g. "10"'/>
+
+      <button type="submit">Submit</button>
+    </form>
+  </>);;
 }
