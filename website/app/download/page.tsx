4a22083e3770778acceb5fc34313da8105fdea08
anshlovescoffee <anandansh76@gmail.com>
2024-04-25 18:22:37 -0500

    Download Page w/ styles

--- website/app/download/page.tsx
+++ website/app/download/page.tsx
@@ -1,22 +1,7 @@
 'use client';
-import React from 'react';
 import './download-styles.css';
 
 export default function DownloadPage() {
-    const handleDownload = async () => {
-        const response = await fetch('/api/download');
-        const blob = await response.blob();
-        const downloadUrl = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = downloadUrl;
-        link.setAttribute('download', 'arduino.ino'); // any other attributes you want to add to the link
-        document.body.appendChild(link);
-        link.click();
-        if (link.parentNode) {
-          link.parentNode.removeChild(link);
-      }
-    };
-
     return (
         <div className="download-container-center">
             <div className='download-container'>
@@ -25,7 +10,13 @@ export default function DownloadPage() {
                 <p className='download-info'>Download the code used on the Arduino Nano ESP32</p>
                 
                 <div className='download-button-center'>
-                    <button onClick={handleDownload} className='download-button'>Download Now</button>
+                    <a 
+                    href="arduino.ino"
+                    download="arduino.ino"
+                    className='download-button'
+                    >
+                    Get File
+                    </a>
                 </div>
                 
             </div>
