9ea1dd079f41b361f4a0e09c933801780b5f534b
Ammar Mohammed <ammar.hm321@gmail.com>
2024-05-06 18:03:55 -0500

    Added comments to describe all the APIs

--- website/app/api/arduino/increment/[id]/route.ts
+++ website/app/api/arduino/increment/[id]/route.ts
@@ -1,3 +1,10 @@
+/*
+This is the Arduino API that is called after the fountain's Arduino senses a bottle has been filled.
+The Arduino is given a URL like: 192.168.0.1/api/arduino/increment/[id]
+Based on the ID in the URL, the API will uss Prisma to alter the SQLite database row 
+and increment that fountain's bottleNum value.
+*/
+
 import prisma from '@/app/prismaFountains';
 import { NextResponse } from 'next/server';
 
--- website/app/api/webapp/create/route.ts
+++ website/app/api/webapp/create/route.ts
@@ -1,3 +1,11 @@
+/*
+This is the Webapp API that is used to Create a fountain in the SQLite database.
+It receives a JSON "Fountain" object containing variables such as "name" and "initial bottle count".
+The fountain object is sent from the user form found on the Create page.
+The API then uses that info to create a new fountain in SQLite database using Prisma.
+The res variable can also give the ID parameter, which will be displayed after form submission.
+*/
+
 import { NextResponse } from "next/server";
 import prisma from '@/app/prismaFountains';
 
